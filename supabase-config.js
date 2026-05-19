b9123b00dcb4a43e09eeed88bef930e8badf74d1
AntionioDrost <t.o.m.drost@student.tudelft.nl>
2026-05-19 13:54:51 +0200

    Connect Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CMP_SUPABASE_CONFIG = {
-  url: "",
-  publishableKey: ""
+  url: "https://wnqucndbikfsuzqnlqde.supabase.co",
+  publishableKey: "sb_publishable_3v5oOqY_GXGLdBAKSvGktw_C12wDivm"
 };
